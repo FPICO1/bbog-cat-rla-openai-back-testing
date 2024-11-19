ea2717d142dc429c46021f4ece5ba0085b0932eb
Antony Acevedo <aaceve2@bancodebogota.com.co>
2024-11-19 15:22:38 -0500

    Credenciales bdDynamo 2

--- src/main/java/com/banbta/bbogcatrlaopenaibacktesting/application/config/DynamoDBConfig.java
+++ src/main/java/com/banbta/bbogcatrlaopenaibacktesting/application/config/DynamoDBConfig.java
@@ -11,11 +11,6 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 @Configuration
 public class DynamoDBConfig {
 
-
-
-
-
-
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
