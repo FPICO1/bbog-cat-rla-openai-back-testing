2986fe7e053d22a5947fcf9a0d8072ee949a9152
Antony Acevedo <aaceve2@bancodebogota.com.co>
2024-11-19 15:28:32 -0500

    Credenciales bdDynamo 5

--- src/main/java/com/banbta/bbogcatrlaopenaibacktesting/application/config/DynamoDBConfig.java
+++ src/main/java/com/banbta/bbogcatrlaopenaibacktesting/application/config/DynamoDBConfig.java
@@ -3,6 +3,7 @@ package com.banbta.bbogcatrlaopenaibacktesting.application.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -13,8 +14,9 @@ public class DynamoDBConfig {
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .region(Region.US_EAST_1) // Asegúrate de que coincide con tu región de AWS
-                       .build();
+                .region(Region.US_EAST_1)
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .build();
     }
 
     @Bean
